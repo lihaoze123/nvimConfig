30e069cee5b0168676182c3ecf1aa20b2afa99ce
chumeng <2595248810@qq.com>
2023-01-29 16:17:55 +0800

    fix coc.nvim

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -20,3 +20,21 @@ opt.smartcase = true
 -- 外观
 opt.signcolumn = "yes"
 vim.cmd[[colorscheme dracula]]
+
+-- Auto Completion
+opt.backup = false
+opt.writebackup = false
+opt.updatetime = 300
+opt.signcolumn = "yes"
+
+local keyset = vim.keymap.set
+
+function _G.check_back_space()
+    local col = vim.fn.col('.') - 1
+    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+end
+
+local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
+keyset("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
+keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
+keyset("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
